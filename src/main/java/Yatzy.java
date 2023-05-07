8ff59e12cb15f8c284242ab96ea3939ffe625674
mohamedaminekallel <kallel.med.amine@gmail.com>
2023-05-07 08:31:44 +0200

    refactor straights and fullHouse

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -92,71 +92,26 @@ public class Yatzy {
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
+        Stream<Integer> dice = Stream.of(d1, d2, d3, d4, d5);
+        Map<Integer, Long> results = dice.collect(groupingBy(die -> die, counting()));
+        if ((results.size() == 5) && (results.containsKey(1))) return 15;
+        else return 0;
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
+        Stream<Integer> dice = Stream.of(d1, d2, d3, d4, d5);
+        Map<Integer, Long> results = dice.collect(groupingBy(die -> die, counting()));
+        if ((results.size() == 5) && (!results.containsKey(1))) return 20;
+        else return 0;
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i + 1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i + 1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
+        Stream<Integer> dice = Stream.of(d1, d2, d3, d4, d5);
+        Map<Integer, Long> results = dice.collect(groupingBy(die -> die, counting()));
+        if (results.size() == 2) return (results.entrySet().stream()
+            .map(e -> e.getKey() * e.getValue())
+            .mapToInt(Long::intValue).sum());
+        else return 0;
     }
 
     private static boolean isYatzy(int d1, int d2, int d3, int d4, int d5) {
--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -1,6 +1,6 @@
-import org.junit.*;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class YatzyTest {
 
